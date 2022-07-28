b7f39f2ba8a8913b6c214ebd83754d937f4e764d
Long.Tran <tranhuuhoanglong248@gmail.com>
2022-07-29 00:17:32 +0700

    flask app calculator

--- calculator.py
+++ calculator.py
@@ -1,13 +1,16 @@
-import sys
+from flask import Flask, request, jsonify
 
-def multi_2_num(num1, num2):
-    return num1 * num2
+app = Flask(__name__)
 
+
+@app.route("/sum")
+def multi_2_num():
+    try:
+        num1 = int(request.args.get('num1'))
+        num2 = int(request.args.get('num2'))
+        return jsonify(num1 * num2)
+    except:
+        raise
 
 if __name__ == "__main__":
-    try:
-        num1 = int(sys.argv[1])
-        num2 = int(sys.argv[2])
-        print(multi_2_num(num1, num2))
-    except Exception:
-        raise
+    app.run(host="0.0.0.0", port=8001, debug=True)
